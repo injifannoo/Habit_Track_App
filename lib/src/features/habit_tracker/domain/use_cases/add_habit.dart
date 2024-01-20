75e82a16bc4742e2e90d36bea60ecc29e22e4942
injifannoo <injifanotu@gmail.com>
2024-01-20 09:30:00 +0300

    habit addition is implemeted

--- lib/src/features/habit_tracker/domain/use_cases/add_habit.dart
+++ lib/src/features/habit_tracker/domain/use_cases/add_habit.dart
@@ -1,5 +1,6 @@
-import 'package:habit_tracker/features/habit_tracker/domain/repositories/habit_repository.dart';
-import 'package:habit_tracker/features/habit_tracker/domain/entities/habit.dart';
+import 'package:habit_tracker_app/src/core/use_cases/use_cases.dart';
+import 'package:habit_tracker_app/src/features/habit_tracker/domain/repositories/habit_repository.dart';
+import 'package:habit_tracker_app/src/features/habit_tracker/domain/entities/habit.dart';
 
 class AddHabit implements UseCase<void, Habit> {
   final HabitRepository repository;
