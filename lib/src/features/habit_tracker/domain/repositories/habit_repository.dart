0bfb4bc6d51a6cc327eaa6eb11cbf72d38632867
injifannoo <injifanotu@gmail.com>
2024-01-23 15:32:00 +0300

    habit repository is implemeted

--- lib/src/features/habit_tracker/domain/repositories/habit_repository.dart
+++ lib/src/features/habit_tracker/domain/repositories/habit_repository.dart
@@ -1,3 +1,5 @@
+import 'package:habit_tracker_app/src/features/habit_tracker/domain/entities/habit.dart';
+
 abstract class HabitRepository {
   Future<List<Habit>> getHabits();
   Future<void> addHabit(Habit habit);
