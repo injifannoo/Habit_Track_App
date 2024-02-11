b2929ecfc7e428e2a958b9e7f2dc0b94e134ce60
injifannoo <injifanotu@gmail.com>
2024-02-11 09:30:00 +0300

    habit tracker screen is implemeted

--- lib/src/features/habit_tracker/presentation/pages/habit_tracker_screen.dart
+++ lib/src/features/habit_tracker/presentation/pages/habit_tracker_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HabitTrackerScreen extends ConsumerWidget {
-  const HabitTrackerScreen({Key? key}) : super(key: key);
+  const HabitTrackerScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
