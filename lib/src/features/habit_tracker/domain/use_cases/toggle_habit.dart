7da8bf9170eabd2f49d1f93fa5949fb69dc2a020
injifannoo <injifanotu@gmail.com>
2024-04-17 09:30:00 +0300

    Toggle habit is implemented

--- lib/src/features/habit_tracker/domain/use_cases/toggle_habit.dart
+++ lib/src/features/habit_tracker/domain/use_cases/toggle_habit.dart
@@ -1,1 +1,14 @@
+import 'package:habit_tracker_app/src/features/habit_tracker/domain/repositories/habit_repository.dart';
+import 'package:habit_tracker_app/src/features/habit_tracker/domain/entities/habit.dart';
+
+class ToggleHabit {
+  final HabitRepository repository;
+
+  ToggleHabit(this.repository);
+
+  Future<void> call(Habit habit) async {
+    final updatedHabit = habit.copyWith(completed: !habit.completed);
+    await repository.updateHabit(updatedHabit);
+  }
+}
 
